08e801e3d78883c1850fd37ede85aa23abfb9330
Abdullah <abdullahkhrief@gmail.com>
2023-05-11 17:38:26 +0300

    Components file and Routes has been set up

---
+++ book-store/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Routes>
+     <Route path='/'/>
+     <Route path='/catigories'/> 
+    </Routes>
+  )
+}
+
+export default App
