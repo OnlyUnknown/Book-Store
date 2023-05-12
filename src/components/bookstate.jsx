ae316f08f0fb07bd093b937ede641ff3048ac697
Abdullah <abdullahkhrief@gmail.com>
2023-05-12 14:29:52 +0300

    Added key to the items and used liters

--- src/components/bookstate.jsx
+++ src/components/bookstate.jsx
@@ -2,24 +2,22 @@ import Book from "./book"
 import AddForm from "./AddForm"
 import { useState } from "react"
 
-const bookarr = 
-    [
-
-]
-
 const BookList = () => {
     const [item] = useState([
         {
+            id:1,
             name:"Eat that frog",
             Author:"Brian Tracy",
             Genre:"Self Improvment"
         },
         {
+            id:2,
             name:"Automic Habits",
             Author:"James Clear",
             Genre:"Self Improvment"   
         },
         {
+            id:3,
             name:"Literary Translation",
             Author:"Hasan Ghazala",
             Genre:"Education"  
@@ -28,8 +26,7 @@ const BookList = () => {
     return(
         <div>
         {item.map((item) => {
-            console.log(item);
-           return( <Book Name={item.name} 
+           return( <Book key={item.id} Name={item.name} 
             Author={item.Author} 
             Genre={item.Genre}/>
            )
