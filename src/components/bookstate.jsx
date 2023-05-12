a07484db5d6de60303121bd3a376c0424b33d8f0
Abdullah <abdullahkhrief@gmail.com>
2023-05-12 14:27:48 +0300

    Book list has been added as a child

--- src/components/bookstate.jsx
+++ src/components/bookstate.jsx
@@ -1,18 +1,49 @@
 import Book from "./book"
 import AddForm from "./AddForm"
+import { useState } from "react"
+
+const bookarr = 
+    [
+
+]
+
+const BookList = () => {
+    const [item] = useState([
+        {
+            name:"Eat that frog",
+            Author:"Brian Tracy",
+            Genre:"Self Improvment"
+        },
+        {
+            name:"Automic Habits",
+            Author:"James Clear",
+            Genre:"Self Improvment"   
+        },
+        {
+            name:"Literary Translation",
+            Author:"Hasan Ghazala",
+            Genre:"Education"  
+        }
+    ])
+    return(
+        <div>
+        {item.map((item) => {
+            console.log(item);
+           return( <Book Name={item.name} 
+            Author={item.Author} 
+            Genre={item.Genre}/>
+           )
+        })}
+        
+        </div>
+    )
+}
+console.log(BookList)
 
 const Bookstate = () => {
     return <>
     <h2>This is Home page</h2>
-    <Book Name="Eat that frog" Author="Brian Tracy"
-    Genre="Self Improvment"
-    />
-    <Book Name="Automic habits" Author="James Clear"
-    Genre="Self Improvment"
-    />
-    <Book Name="Literary Translation" Author="Hasan ghazala"
-    Genre="Education"
-    />
+    <BookList/>
     <br/>
     <br/>
     <AddForm/>
