d74757c18dddbe62ec6225c40e5094119b493466
Abdullah <abdullahkhrief@gmail.com>
2023-05-16 18:24:44 +0300

    Add and remove book features has been added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
+import { store } from './redux/store.jsx';
+import {addBook,removeBook} from './redux/books/booksSlice.jsx';
+
+
+
+store.dispatch(addBook("Eat That Frog"))
+store.dispatch(addBook("Automic Habits"))
+store.dispatch(removeBook("Eat That Frog"))
+unsubscribe()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
