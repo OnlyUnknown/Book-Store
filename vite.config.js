fc2cc18b68f8e21dab7f784791a8c9e27dd33b57
OnlyUnknown <abdullahkhrief@gmail.com>
2023-10-08 13:21:56 +0300

    have added some changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/book-store",
   plugins: [react()],
-  build: {
-    outDir: 'dist', // or your preferred output directory
-  },
 })
